faa3aed2373bdfa52d5f821fdf3a0a9c0d25e52f
Lunarian3 <71202068+LunarianCubed@users.noreply.github.com>
2023-02-25 03:48:49 -0600

    Alternated getNextToken from advanced to a basic mode to fit Assignment requirements

--- src/LexicalAnalysis/Lexer.java
+++ src/LexicalAnalysis/Lexer.java
@@ -5,39 +5,100 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 enum Token {
-    // Keywords
-    IF, FOR, WHILE, PROC, RETURN, INT, ELSE, DO, BREAK, END, ASSIGN, ADD_OP, SUB_OP, MUL_OP, DIV_OP, MOD_OP, GT, LT, GE, LE, INC, LP, RP, LB, RB, OR, AND, EE, NEG, COMMA, SEMI, INT_CONST, IDENT
+    IF, FOR, WHILE, PROC, RETURN, INT, ELSE, DO, BREAK, END, ASSIGN, ADD_OP, SUB_OP, MUL_OP, DIV_OP, MOD_OP, GT, LT, GE, LE, INC, LP, RP, LB, RB, OR, AND, EE, NEG, COMMA, SEMI, INT_CONST, STR_CONST, IDENT
 }
+//Mingyang Li
+//898137443
+
 
 public class Lexer {
 
+
+
     private static String line;
     private static int index;
+//    private static int lineNum = 0;
     private static boolean isComment = false;
+//    private static boolean isString = false;
 
     public static void tokenize(String fileName) throws FileNotFoundException {
         File file = new File(fileName);
         Scanner sc = new Scanner(file);
         while (sc.hasNextLine()) {
-            String line = sc.nextLine();
+            line = sc.nextLine();
+//            lineNum++;
+//                                                                                                                System.out.println(line);
+            if(line.length() == 0) continue;
+            deleteEndSpace();
             for(index = 0; index < line.length(); index++) {
                 skipWhiteSpace();
-
-
+                printToken(getNextToken(getNextSymbol()));
             }
         }
     }
 
     public static String getNextSymbol() {
-        return "" + line.charAt(index);
-    }
-
-    public static void error(){
-        System.err.println("Error");
+        String sym =  "" + line.charAt(index);
+        if (Character.isLetter(sym.charAt(0))){
+            while ((index<line.length()-1)&&Character.isLetterOrDigit(line.charAt(index + 1))) {
+                index++;
+                sym += line.charAt(index);
+            }
+        }else if(sym.equals(">") || sym.equals("<") || sym.equals("!") || sym.equals("=")) {
+            if (line.charAt(index + 1) == '=') {
+                index++;
+                sym += line.charAt(index);
+            }
+        } else if (sym.equals("+")) {
+            if(line.charAt(index + 1) == '+') {
+                index++;
+                sym += line.charAt(index);
+            }
+//        } else if (sym.equals("|")) {
+//            if(line.charAt(index + 1) == '|') {
+//                index++;
+//                sym += line.charAt(index);
+//            }
+//        } else if (sym.equals("&")) {
+//            if(line.charAt(index + 1) == '&') {
+//                index++;
+//                sym += line.charAt(index);
+//            }
+        } else if (sym.equals("/")) {
+            if(line.charAt(index + 1) == '/') {
+                index++;
+                sym += line.charAt(index);
+            } else if(line.charAt(index + 1) == '*') {
+                index++;
+                sym += line.charAt(index);
+            }
+        } else if (sym.equals("\"")) {
+            while (line.charAt(index + 1) != '"') {
+                index++;
+                sym += line.charAt(index);
+                if(index == line.length() - 2) error("STRING NOT CLOSED");
+            }
+            sym += line.charAt(index + 1);
+            index++;
+        } else if (sym.equals("*")) {
+            if(line.charAt(index + 1) == '/') {
+                index++;
+                sym += line.charAt(index);
+            }
+        }
+//                                                                                                            System.out.println("index = " + index);
+//                                                                                                            System.out.println(sym);
+        return sym;
     }
 
     public static Token getNextToken(String symbol) {
-
+        //Can't deal with /* */ in multiple lines for now
+        if(isComment) {
+            if(symbol.equals("*/")){
+                isComment = false;
+            }
+            return null;
+        }
         switch (symbol) {
             case "if":
                 return Token.IF;
@@ -45,7 +106,7 @@ public class Lexer {
                 return Token.FOR;
             case "while":
                 return Token.WHILE;
-            case "proc":
+            case "procedure":
                 return Token.PROC;
             case "return":
                 return Token.RETURN;
@@ -59,20 +120,10 @@ public class Lexer {
                 return Token.BREAK;
             case "end":
                 return Token.END;
-            case "=":{
-                if(line.charAt(index+1) == '='){
-                    index++;
-                    return Token.EE;
-                }
+            case "=":
                 return Token.ASSIGN;
-            }
-            case "+":{
-                if(line.charAt(index+1) == '+'){
-                    index++;
-                    return Token.INC;
-                }
+            case "+":
                 return Token.ADD_OP;
-            }
             case "-":
                 return Token.SUB_OP;
             case "*":
@@ -95,13 +146,17 @@ public class Lexer {
                 return Token.LP;
             case ")":
                 return Token.RP;
-            case "[":
+            case "{":
                 return Token.LB;
-            case "]":
+            case "}":
                 return Token.RB;
-            case "||":
+//            case "||":
+//                return Token.OR;
+//            case "&&":
+//                return Token.AND;
+            case "|":
                 return Token.OR;
-            case "&&":
+            case "&":
                 return Token.AND;
             case "==":
                 return Token.EE;
@@ -114,32 +169,43 @@ public class Lexer {
             case "/*":
             {
                 isComment = true;
+                return null;
             }
             case "//":
             {
-
+                line = line.substring(0, index-1);
+                return null;
+            }
+            case "\"":
+            {
+//                isString = true;
+                return Token.STR_CONST;
             }
             default: {
-                if (Character.isDigit(symbol.charAt(0))) {
-                    while (Character.isDigit(line.charAt(index + 1))) {
+                if(symbol.charAt(0) == '"') return Token.STR_CONST;
+                else if (Character.isDigit(symbol.charAt(0))) {
+                    if(Character.isLetter(line.charAt(index + 1))) error("INVALID IDENTIFIER NAME");
+                    while ((index<line.length()-1)&&Character.isDigit(line.charAt(index + 1))) {
                         index++;
-                        symbol += line.charAt(index);
                     }
                     return Token.INT_CONST;
                 } else if (Character.isLetter(symbol.charAt(0))) {
-                    while (Character.isLetterOrDigit(line.charAt(index + 1))) {
+                    while ((index<line.length()-1)&&Character.isLetterOrDigit(line.charAt(index + 1))) {
                         index++;
-                        symbol += line.charAt(index);
                     }
                     return Token.IDENT;
                 } else {
-                    error();
+                    error("INVALID SYMBOL");
                     return null;
                 }
             }
         }
     }
 
+    public static void error(String msg){
+        System.err.print("SYNTAX ERROR: " + msg);
+        System.exit(1);
+    }
 
     public static void skipWhiteSpace() {
         while (index < line.length() && Character.isWhitespace(line.charAt(index))) {
@@ -147,18 +213,13 @@ public class Lexer {
         }
     }
 
-    public void skipComment(String symbol) {
-        if (symbol.equals("/")) {
-            isComment = true;
-        }
-        if (isComment) {
-            if (symbol.equals("*")) {
-                isComment = false;
-            }
+    public static void deleteEndSpace(){
+        while(line.charAt(line.length()-1) == ' '){
+            line = line.substring(0, line.length()-1);
         }
     }
 
     public static void printToken(Token token) {
-        System.out.println(token);
+        if(token != null) System.out.println(token);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import LexicalAnalysis.Lexer;
 
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter file name: ");
+        String fileName = sc.nextLine();
         try {
-            Lexer.tokenize("test.txt");
+            Lexer.tokenize(fileName);
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
